75cb0778b4230fff903c8556982f0c77a9ffe5e9
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-02-09 14:11:38 +0200

    v.pradine: pridejau pradini koda

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using std::left;
+using std::setw;
+
+struct Studentas {
+    string Vardas = "A";
+    string Pavarde = "BB";
+    vector<int> paz;
+    int exam;
+    double rez;
+};
+
+int main() {
+    return 0;
+}
+
+
