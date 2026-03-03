7de4d02ac5641a33f6792a1121788cbb87b63037
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-03-03 13:54:07 +0200

    v0.3: pridetas try/catch failu nuskaitymui ir isvedimui Failai.cpp

--- Failai.cpp
+++ Failai.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <exception>
 
 using std::ifstream;
 using std::ofstream;
@@ -17,88 +18,103 @@ using std::setprecision;
 using std::endl;
 
 bool nuskaityti_is_failo(const string &failas, vector<Studentas> &grupe, int &praleista) {
-    ifstream in(failas);
-    if (!in) {
-        std::cout << "Nepavyko atidaryti failo: " << failas << endl;
-        return false;
-    }
-
     grupe.clear();
     praleista = 0;
 
-    string line;
-    while (getline(in, line)) {
-        if (line.empty()) continue;
+    try {
+        ifstream in;
+        in.exceptions(std::ios::failbit | std::ios::badbit);
+        in.open(failas);
 
-        istringstream iss(line);
+        string line;
+        while (getline(in, line)) {
+            if (line.empty()) continue;
 
-        Studentas a;
-        if (!(iss >> a.vardas >> a.pavarde)) {
-            praleista++;
-            continue;
-        }
+            istringstream iss(line);
 
-        if (a.vardas == "Vardas" && a.pavarde == "Pavarde") continue;
-
-        vector<int> skaiciai;
-        int x;
-        while (iss >> x) skaiciai.push_back(x);
-
-        if (!iss.eof()) {
-            praleista++;
-            continue;
-        }
-
-        if (skaiciai.size() < 2) {
-            praleista++;
-            continue;
-        }
-
-        bool bloga = false;
-        for (int v : skaiciai) {
-            if (v < 1 || v > 10) {
-                bloga = true;
-                break;
+            Studentas a;
+            if (!(iss >> a.vardas >> a.pavarde)) {
+                praleista++;
+                continue;
             }
+
+            if (a.vardas == "Vardas" && a.pavarde == "Pavarde") continue;
+
+            vector<int> skaiciai;
+            int x;
+            while (iss >> x) skaiciai.push_back(x);
+
+            // jei liko ne skaiciai (pvz. raide), laikom eilute bloga
+            if (!iss.eof()) {
+                praleista++;
+                continue;
+            }
+
+            if (skaiciai.size() < 2) {
+                praleista++;
+                continue;
+            }
+
+            bool bloga = false;
+            for (int v : skaiciai) {
+                if (v < 1 || v > 10) {
+                    bloga = true;
+                    break;
+                }
+            }
+
+            if (bloga) {
+                praleista++;
+                continue;
+            }
+
+            a.egz = skaiciai.back();
+            skaiciai.pop_back();
+            a.paz = skaiciai;
+
+            skaiciuoti(a);
+            grupe.push_back(a);
         }
 
-        if (bloga) {
-            praleista++;
-            continue;
-        }
-
-        a.egz = skaiciai.back();
-        skaiciai.pop_back();
-        a.paz = skaiciai;
-
-        skaiciuoti(a);
-        grupe.push_back(a);
+        return true;
+    } catch (const std::ios_base::failure &) {
+        std::cout << "Klaida: failas neegzistuoja arba nepavyko jo atidaryti: " << failas << endl;
+        grupe.clear();
+        praleista = 0;
+        return false;
+    } catch (const std::exception &) {
+        std::cout << "Klaida: ivyko nenumatyta klaida skaitant faila.\n";
+        grupe.clear();
+        praleista = 0;
+        return false;
     }
-
-    return true;
 }
 
 void isvesti_i_faila(const vector<Studentas> &grupe, const string &failas) {
-    ofstream out(failas);
-    if (!out) {
-        std::cout << "Nepavyko sukurti failo.\n";
-        return;
-    }
+    try {
+        ofstream out;
+        out.exceptions(std::ios::failbit | std::ios::badbit);
+        out.open(failas);
 
-    out << left << setw(15) << "Vardas"
-        << setw(20) << "Pavarde"
-        << setw(18) << "Galutinis (Vid.)"
-        << setw(18) << "Galutinis (Med.)"
-        << "\n";
-
-    out << string(15 + 20 + 18 + 18, '-') << "\n";
-
-    out << fixed << setprecision(2);
-    for (const auto &a : grupe) {
-        out << left << setw(15) << a.vardas
-            << setw(20) << a.pavarde
-            << setw(18) << a.gal_vid
-            << setw(18) << a.gal_med
+        out << left << setw(15) << "Vardas"
+            << setw(20) << "Pavarde"
+            << setw(18) << "Galutinis (Vid.)"
+            << setw(18) << "Galutinis (Med.)"
             << "\n";
+
+        out << string(15 + 20 + 18 + 18, '-') << "\n";
+
+        out << fixed << setprecision(2);
+        for (const auto &a : grupe) {
+            out << left << setw(15) << a.vardas
+                << setw(20) << a.pavarde
+                << setw(18) << a.gal_vid
+                << setw(18) << a.gal_med
+                << "\n";
+        }
+    } catch (const std::ios_base::failure &) {
+        std::cout << "Klaida: nepavyko sukurti arba irasyti i faila: " << failas << endl;
+    } catch (const std::exception &) {
+        std::cout << "Klaida: ivyko nenumatyta klaida rasant i faila.\n";
     }
 }
