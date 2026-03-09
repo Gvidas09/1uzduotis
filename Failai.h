03e0a079e7a4b5150a6758ccb4495500b144e4ad
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-03-09 12:19:31 +0200

    v0.4: pridėtos failų generavimo ir studentų skirstymo funkcijos

--- Failai.h
+++ Failai.h
@@ -5,3 +5,11 @@
 
 bool nuskaityti_is_failo(const std::string& failas, std::vector<Studentas>& grupe, int& praleista);
 void isvesti_i_faila(const std::vector<Studentas>& grupe, const std::string& failas);
+
+bool generuoti_studentu_faila(const std::string& failas, int kiek_studentu, int kiek_nd);
+void padalinti_studentus(const std::vector<Studentas>& visi,
+                         std::vector<Studentas>& vargsiukai,
+                         std::vector<Studentas>& kietiakiai);
+void sudaryti_rezultatu_failu_vardus(const std::string& pradinis_failas,
+                                     std::string& vargsiuku_failas,
+                                     std::string& kietiaku_failas);
