d0f480c153a58952c513ca66f37299b0b0bb7d06
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-03-09 14:10:01 +0200

    v0.4: realizuotas rikiavimas pagal pasirinktą kriterijų

--- Rikiavimas.cpp
+++ Rikiavimas.cpp
@@ -12,7 +12,7 @@ bool ar_skaitmuo(char c) {
     return c >= '0' && c <= '9';
 }
 
-int skaicius_gale(const string &s) {
+int skaicius_gale(const string& s) {
     int i = (int)s.size() - 1;
     if (i < 0) return -1;
     if (!ar_skaitmuo(s[i])) return -1;
@@ -28,7 +28,7 @@ int skaicius_gale(const string &s) {
     return sk;
 }
 
-string tekstas_be_galo_skaiciaus(const string &s) {
+string tekstas_be_galo_skaiciaus(const string& s) {
     int i = (int)s.size() - 1;
     if (i < 0) return s;
     if (!ar_skaitmuo(s[i])) return s;
@@ -37,7 +37,7 @@ string tekstas_be_galo_skaiciaus(const string &s) {
     return s.substr(0, i + 1);
 }
 
-bool palyginti_nat(const string &a, const string &b) {
+bool palyginti_nat(const string& a, const string& b) {
     string ta = tekstas_be_galo_skaiciaus(a);
     string tb = tekstas_be_galo_skaiciaus(b);
 
@@ -51,44 +51,48 @@ bool palyginti_nat(const string &a, const string &b) {
     return a < b;
 }
 
-bool palyginti_pagal_varda(const Studentas &a, const Studentas &b) {
+bool palyginti_pagal_varda(const Studentas& a, const Studentas& b) {
     if (a.vardas != b.vardas) return palyginti_nat(a.vardas, b.vardas);
     return palyginti_nat(a.pavarde, b.pavarde);
 }
 
-bool palyginti_pagal_pavarde(const Studentas &a, const Studentas &b) {
+bool palyginti_pagal_pavarde(const Studentas& a, const Studentas& b) {
     if (a.pavarde != b.pavarde) return palyginti_nat(a.pavarde, b.pavarde);
     return palyginti_nat(a.vardas, b.vardas);
 }
 
-bool palyginti_pagal_vidurki(const Studentas &a, const Studentas &b) {
+bool palyginti_pagal_vidurki(const Studentas& a, const Studentas& b) {
     if (a.gal_vid != b.gal_vid) return a.gal_vid < b.gal_vid;
     if (a.pavarde != b.pavarde) return palyginti_nat(a.pavarde, b.pavarde);
     return palyginti_nat(a.vardas, b.vardas);
 }
 
-bool palyginti_pagal_mediana(const Studentas &a, const Studentas &b) {
+bool palyginti_pagal_mediana(const Studentas& a, const Studentas& b) {
     if (a.gal_med != b.gal_med) return a.gal_med < b.gal_med;
     if (a.pavarde != b.pavarde) return palyginti_nat(a.pavarde, b.pavarde);
     return palyginti_nat(a.vardas, b.vardas);
 }
 
-void rikiuoti(vector<Studentas> &grupe) {
+void rikiuoti(vector<Studentas>& grupe, int kriterijus) {
+    if (grupe.empty()) return;
+
+    if (kriterijus == 1) {
+        sort(grupe.begin(), grupe.end(), palyginti_pagal_varda);
+    } else if (kriterijus == 2) {
+        sort(grupe.begin(), grupe.end(), palyginti_pagal_pavarde);
+    } else if (kriterijus == 3) {
+        sort(grupe.begin(), grupe.end(), palyginti_pagal_vidurki);
+    } else if (kriterijus == 4) {
+        sort(grupe.begin(), grupe.end(), palyginti_pagal_mediana);
+    }
+}
+
+void rikiuoti(vector<Studentas>& grupe) {
     if (grupe.empty()) {
         std::cout << "Grupe tuscia - nera ka rikiuoti.\n";
         return;
     }
 
-    std::cout << "\nRikiuoti studentus pagal:\n";
-    std::cout << "1 - Varda\n";
-    std::cout << "2 - Pavarde\n";
-    std::cout << "3 - Galutini (Vid.)\n";
-    std::cout << "4 - Galutini (Med.)\n";
-
-    int r = ivesti_skaiciu("Pasirinkimas: ", 1, 4);
-
-    if (r == 1) sort(grupe.begin(), grupe.end(), palyginti_pagal_varda);
-    else if (r == 2) sort(grupe.begin(), grupe.end(), palyginti_pagal_pavarde);
-    else if (r == 3) sort(grupe.begin(), grupe.end(), palyginti_pagal_vidurki);
-    else sort(grupe.begin(), grupe.end(), palyginti_pagal_mediana);
+    int r = pasirinkti_rikiavimo_kriteriju();
+    rikiuoti(grupe, r);
 }
