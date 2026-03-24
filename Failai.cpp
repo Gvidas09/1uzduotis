b3283f78ee686283abbc9fa4bc4c55ea34cdec0f
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-03-24 22:44:47 +0200

    Suvienodinau failų skaitymą ir rašymą, kad būtų galima naudoti su skirtingais konteineriais

--- Failai.cpp
+++ Failai.cpp
@@ -1,142 +1,51 @@
-#include <iostream>
 #include "Failai.h"
-#include <fstream>
 #include <sstream>
-#include <iomanip>
-#include <exception>
 
-using std::ifstream;
-using std::ofstream;
-using std::istringstream;
-using std::string;
-using std::vector;
-using std::getline;
-using std::left;
-using std::setw;
-using std::fixed;
-using std::setprecision;
-using std::endl;
+bool nuskaityti_studenta_is_eilutes(const std::string& eilute, Studentas& a, bool& praleisti) {
+    praleisti = false;
 
-bool nuskaityti_is_failo(const string& failas, vector<Studentas>& grupe, int& praleista) {
-    grupe.clear();
-    praleista = 0;
-
-    try {
-        ifstream in;
-        in.exceptions(std::ios::badbit);
-        in.open(failas);
-
-        if (!in.is_open()) {
-            std::cout << "Klaida: failas neegzistuoja arba nepavyko jo atidaryti: " << failas << endl;
-            return false;
-        }
-
-        string line;
-        while (getline(in, line)) {
-            if (line.empty()) continue;
-
-            istringstream iss(line);
-
-            Studentas a;
-            if (!(iss >> a.vardas >> a.pavarde)) {
-                praleista++;
-                continue;
-            }
-
-            if (a.vardas == "Vardas" && a.pavarde == "Pavarde") continue;
-
-            vector<int> skaiciai;
-            int x;
-            while (iss >> x) skaiciai.push_back(x);
-
-            if (!iss.eof()) {
-                praleista++;
-                continue;
-            }
-
-            if (skaiciai.size() < 2) {
-                praleista++;
-                continue;
-            }
-
-            bool bloga = false;
-            for (int v : skaiciai) {
-                if (v < 1 || v > 10) {
-                    bloga = true;
-                    break;
-                }
-            }
-
-            if (bloga) {
-                praleista++;
-                continue;
-            }
-
-            a.egz = skaiciai.back();
-            skaiciai.pop_back();
-            a.paz = skaiciai;
-
-            skaiciuoti(a);
-            grupe.push_back(a);
-        }
-
-        if (in.bad()) {
-            std::cout << "Klaida: ivyko skaitymo klaida (badbit) skaitant faila: " << failas << endl;
-            grupe.clear();
-            praleista = 0;
-            return false;
-        }
-
-        return true;
-    }
-    catch (const std::ios_base::failure&) {
-        std::cout << "Klaida: ivyko I/O klaida skaitant faila: " << failas << endl;
-        grupe.clear();
-        praleista = 0;
+    std::istringstream iss(eilute);
+    if (!(iss >> a.vardas >> a.pavarde)) {
+        praleisti = true;
         return false;
     }
-    catch (const std::exception&) {
-        std::cout << "Klaida: ivyko nenumatyta klaida skaitant faila.\n";
-        grupe.clear();
-        praleista = 0;
+
+    if (a.vardas == "Vardas" && a.pavarde == "Pavarde") {
         return false;
     }
-}
 
-void isvesti_i_faila(const vector<Studentas>& grupe, const string& failas) {
-    try {
-        ofstream out;
-        out.exceptions(std::ios::failbit | std::ios::badbit);
-        out.open(failas);
+    std::vector<int> skaiciai;
+    int x = 0;
+    while (iss >> x) skaiciai.push_back(x);
 
-        out << left << setw(15) << "Vardas"
-            << setw(20) << "Pavarde"
-            << setw(18) << "Galutinis (Vid.)"
-            << setw(18) << "Galutinis (Med.)"
-            << "\n";
+    if (!iss.eof()) {
+        praleisti = true;
+        return false;
+    }
 
-        out << string(15 + 20 + 18 + 18, '-') << "\n";
+    if (skaiciai.size() < 2) {
+        praleisti = true;
+        return false;
+    }
 
-        out << fixed << setprecision(2);
-        for (const auto& a : grupe) {
-            out << left << setw(15) << a.vardas
-                << setw(20) << a.pavarde
-                << setw(18) << a.gal_vid
-                << setw(18) << a.gal_med
-                << "\n";
+    for (int v : skaiciai) {
+        if (v < 1 || v > 10) {
+            praleisti = true;
+            return false;
         }
     }
-    catch (const std::ios_base::failure&) {
-        std::cout << "Klaida: nepavyko sukurti arba irasyti i faila: " << failas << endl;
-    }
-    catch (const std::exception&) {
-        std::cout << "Klaida: ivyko nenumatyta klaida rasant i faila.\n";
-    }
+
+    a.egz = skaiciai.back();
+    skaiciai.pop_back();
+    a.paz = std::move(skaiciai);
+    skaiciuoti(a);
+
+    return true;
 }
 
-bool generuoti_studentu_faila(const string& failas, int kiek_studentu, int kiek_nd) {
+bool generuoti_studentu_faila(const std::string& failas, int kiek_studentu, int kiek_nd) {
     try {
-        ofstream out;
+        std::ofstream out;
         out.exceptions(std::ios::failbit | std::ios::badbit);
         out.open(failas);
 
@@ -160,7 +69,7 @@ bool generuoti_studentu_faila(const string& failas, int kiek_studentu, int kiek_
         return true;
     }
     catch (const std::ios_base::failure&) {
-        std::cout << "Klaida: nepavyko sugeneruoti failo: " << failas << endl;
+        std::cout << "Klaida: nepavyko sugeneruoti failo: " << failas << "\n";
         return false;
     }
     catch (const std::exception&) {
@@ -169,35 +78,14 @@ bool generuoti_studentu_faila(const string& failas, int kiek_studentu, int kiek_
     }
 }
 
-void padalinti_studentus(const vector<Studentas>& visi,
-                         vector<Studentas>& vargsiukai,
-                         vector<Studentas>& kietiakiai) {
-    vargsiukai.clear();
-    kietiakiai.clear();
-
-    vargsiukai.reserve(visi.size());
-    kietiakiai.reserve(visi.size());
-
-    for (const auto& s : visi) {
-        if (s.gal_vid < 5.0) {
-            vargsiukai.push_back(s);
-        } else {
-            kietiakiai.push_back(s);
-        }
-    }
-}
-
-void sudaryti_rezultatu_failu_vardus(const string& pradinis_failas,
-                                     string& vargsiuku_failas,
-                                     string& kietiaku_failas) {
-    size_t taskas = pradinis_failas.rfind('.');
-
-    if (taskas == string::npos) {
+void sudaryti_rezultatu_failu_vardus(const std::string& pradinis_failas, std::string& vargsiuku_failas, std::string& kietiaku_failas) {
+    std::size_t taskas = pradinis_failas.rfind('.');
+    if (taskas == std::string::npos) {
         vargsiuku_failas = pradinis_failas + "_vargsiukai.txt";
         kietiaku_failas = pradinis_failas + "_kietiakiai.txt";
     } else {
-        string pagrindas = pradinis_failas.substr(0, taskas);
-        string galune = pradinis_failas.substr(taskas);
+        std::string pagrindas = pradinis_failas.substr(0, taskas);
+        std::string galune = pradinis_failas.substr(taskas);
 
         vargsiuku_failas = pagrindas + "_vargsiukai" + galune;
         kietiaku_failas = pagrindas + "_kietiakiai" + galune;
--- Failai.h
+++ Failai.h
@@ -1,15 +1,116 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <list>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <exception>
+
 #include "Studentas.h"
 
-bool nuskaityti_is_failo(const std::string& failas, std::vector<Studentas>& grupe, int& praleista);
-void isvesti_i_faila(const std::vector<Studentas>& grupe, const std::string& failas);
-
+bool nuskaityti_studenta_is_eilutes(const std::string& eilute, Studentas& a, bool& praleisti);
 bool generuoti_studentu_faila(const std::string& failas, int kiek_studentu, int kiek_nd);
-void padalinti_studentus(const std::vector<Studentas>& visi,
-                         std::vector<Studentas>& vargsiukai,
-                         std::vector<Studentas>& kietiakiai);
-void sudaryti_rezultatu_failu_vardus(const std::string& pradinis_failas,
-                                     std::string& vargsiuku_failas,
-                                     std::string& kietiaku_failas);
+void sudaryti_rezultatu_failu_vardus(const std::string& pradinis_failas, std::string& vargsiuku_failas, std::string& kietiaku_failas);
+
+template <typename Container>
+bool nuskaityti_is_failo(const std::string& failas, Container& grupe, int& praleista) {
+    grupe.clear();
+    praleista = 0;
+
+    try {
+        std::ifstream in;
+        in.exceptions(std::ios::badbit);
+        in.open(failas);
+
+        if (!in.is_open()) {
+            std::cout << "Klaida: failas neegzistuoja arba nepavyko jo atidaryti: " << failas << "\n";
+            return false;
+        }
+
+        std::string line;
+        while (std::getline(in, line)) {
+            if (line.empty()) continue;
+
+            Studentas a;
+            bool atmesti = false;
+            if (!nuskaityti_studenta_is_eilutes(line, a, atmesti)) {
+                if (atmesti) praleista++;
+                continue;
+            }
+
+            grupe.push_back(a);
+        }
+
+        if (in.bad()) {
+            std::cout << "Klaida: ivyko skaitymo klaida (badbit) skaitant faila: " << failas << "\n";
+            grupe.clear();
+            praleista = 0;
+            return false;
+        }
+
+        return true;
+    }
+    catch (const std::ios_base::failure&) {
+        std::cout << "Klaida: ivyko I/O klaida skaitant faila: " << failas << "\n";
+        grupe.clear();
+        praleista = 0;
+        return false;
+    }
+    catch (const std::exception&) {
+        std::cout << "Klaida: ivyko nenumatyta klaida skaitant faila.\n";
+        grupe.clear();
+        praleista = 0;
+        return false;
+    }
+}
+
+template <typename Container>
+bool isvesti_i_faila(const Container& grupe, const std::string& failas) {
+    try {
+        std::ofstream out;
+        out.exceptions(std::ios::failbit | std::ios::badbit);
+        out.open(failas);
+
+        out << std::left << std::setw(15) << "Vardas"
+            << std::setw(20) << "Pavarde"
+            << std::setw(18) << "Galutinis (Vid.)"
+            << std::setw(18) << "Galutinis (Med.)"
+            << "\n";
+
+        out << std::string(15 + 20 + 18 + 18, '-') << "\n";
+
+        out << std::fixed << std::setprecision(2);
+        for (const auto& a : grupe) {
+            out << std::left << std::setw(15) << a.vardas
+                << std::setw(20) << a.pavarde
+                << std::setw(18) << a.gal_vid
+                << std::setw(18) << a.gal_med
+                << "\n";
+        }
+
+        return true;
+    }
+    catch (const std::ios_base::failure&) {
+        std::cout << "Klaida: nepavyko sukurti arba irasyti i faila: " << failas << "\n";
+        return false;
+    }
+    catch (const std::exception&) {
+        std::cout << "Klaida: ivyko nenumatyta klaida rasant i faila.\n";
+        return false;
+    }
+}
+
+inline void padalinti_studentus(const std::vector<Studentas>& visi, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietiakiai) {
+    vargsiukai.clear();
+    kietiakiai.clear();
+
+    for (const auto& s : visi) {
+        if (s.gal_vid < 5.0) {
+            vargsiukai.push_back(s);
+        } else {
+            kietiakiai.push_back(s);
+        }
+    }
+}
