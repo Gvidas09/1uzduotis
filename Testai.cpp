74a69a1728b3db5332b40cfbf0e71d788c56b8c5
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-04-21 20:44:07 +0300

    v1.2: uzbaigti v12 testai rule of five ir operatoriams

--- Testai.cpp
+++ Testai.cpp
@@ -2,6 +2,7 @@
 #include "Studentas.h"
 #include <iostream>
 #include <sstream>
+#include <utility>
 
 namespace {
 
@@ -9,24 +10,62 @@ void spausdinti_rezultata(const std::string& testo_pavadinimas, bool pavyko) {
     std::cout << (pavyko ? "[OK] " : "[NEPAVYKO] ") << testo_pavadinimas << "\n";
 }
 
+bool testuoti_numatytaji_konstruktoriu() {
+    Studentas s;
+    return s.vardas().empty() && s.pavarde().empty() && s.pazymiai().empty() &&
+           s.egzaminas() == 0 && s.galutinisVid() == 0.0 && s.galutinisMed() == 0.0;
+}
+
+bool testuoti_parametru_konstruktoriu() {
+    Studentas s("Jonas", "Jonaitis", {8, 9, 10}, 9);
+    return s.vardas() == "Jonas" && s.pavarde() == "Jonaitis" &&
+           s.egzaminas() == 9 && !s.pazymiai().empty();
+}
+
+bool testuoti_kopijavimo_konstruktoriu() {
+    Studentas a("Ona", "Onaite", {7, 8, 9}, 10);
+    Studentas b(a);
+    return b.vardas() == a.vardas() && b.pavarde() == a.pavarde() &&
+           b.pazymiai() == a.pazymiai() && b.egzaminas() == a.egzaminas();
+}
+
+bool testuoti_perkelimo_konstruktoriu() {
+    Studentas a("Petras", "Petraitis", {6, 7, 8}, 9);
+    Studentas b(std::move(a));
+    return b.vardas() == "Petras" && b.pavarde() == "Petraitis" &&
+           a.vardas().empty() && a.pavarde().empty() && a.pazymiai().empty();
+}
+
+bool testuoti_kopijavimo_priskyrima() {
+    Studentas a("Ieva", "Ievaite", {10, 9, 8}, 9);
+    Studentas b;
+    b = a;
+    return b.vardas() == a.vardas() && b.pavarde() == a.pavarde() &&
+           b.pazymiai() == a.pazymiai() && b.egzaminas() == a.egzaminas();
+}
+
+bool testuoti_perkelimo_priskyrima() {
+    Studentas a("Matas", "Matutis", {5, 6, 7}, 8);
+    Studentas b;
+    b = std::move(a);
+    return b.vardas() == "Matas" && b.pavarde() == "Matutis" &&
+           a.vardas().empty() && a.pavarde().empty() && a.pazymiai().empty();
+}
+
 bool testuoti_isvesties_operatoriu() {
     Studentas s("Aiste", "Aistaite", {10, 9, 8}, 10);
     std::ostringstream out;
     out << s;
     std::string tekstas = out.str();
-    return tekstas.find("Aiste") != std::string::npos &&
-           tekstas.find("Aistaite") != std::string::npos;
+    return tekstas.find("Aiste") != std::string::npos && tekstas.find("Aistaite") != std::string::npos;
 }
 
 bool testuoti_ivedimo_operatoriu_is_failo_formato() {
     std::istringstream in("Jonas Jonaitis 8 9 10 9\n");
     Studentas s;
     in >> s;
-    return in.good() &&
-           s.vardas() == "Jonas" &&
-           s.pavarde() == "Jonaitis" &&
-           s.pazymiai().size() == 3 &&
-           s.egzaminas() == 9;
+    return in.good() && s.vardas() == "Jonas" && s.pavarde() == "Jonaitis" &&
+           s.pazymiai().size() == 3 && s.egzaminas() == 9;
 }
 
 bool testuoti_ivedimo_operatoriu_su_blogais_duomenimis() {
@@ -39,11 +78,17 @@ bool testuoti_ivedimo_operatoriu_su_blogais_duomenimis() {
 } // namespace
 
 void vykdyti_v12_testus() {
-    std::cout << "\nV1.2 TESTAI - ivesties ir isvesties operatoriai\n\n";
+    std::cout << "\nV1.2 TESTAI - Rule of Five ir ivesties/isvesties operatoriai\n\n";
 
+    spausdinti_rezultata("Numatytasis konstruktorius", testuoti_numatytaji_konstruktoriu());
+    spausdinti_rezultata("Konstruktorius su parametrais", testuoti_parametru_konstruktoriu());
+    spausdinti_rezultata("Kopijavimo konstruktorius", testuoti_kopijavimo_konstruktoriu());
+    spausdinti_rezultata("Perkelimo konstruktorius", testuoti_perkelimo_konstruktoriu());
+    spausdinti_rezultata("Kopijavimo priskyrimo operatorius", testuoti_kopijavimo_priskyrima());
+    spausdinti_rezultata("Perkelimo priskyrimo operatorius", testuoti_perkelimo_priskyrima());
     spausdinti_rezultata("Isvesties operatorius <<", testuoti_isvesties_operatoriu());
     spausdinti_rezultata("Ivesties operatorius >> is failo formato", testuoti_ivedimo_operatoriu_is_failo_formato());
     spausdinti_rezultata("Ivesties operatorius >> su blogais duomenimis", testuoti_ivedimo_operatoriu_su_blogais_duomenimis());
 
-    std::cout << "\nPradiniai v1.2 testai baigti.\n";
+    std::cout << "\nV1.2 testai baigti.\n";
 }
