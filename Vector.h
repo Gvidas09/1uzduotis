9eb3ab3a072fccaf91f5a7c7d570523d1365f124
Gvidas09 <kucinskas.gvidas@gmail.com>
2026-05-14 14:52:31 +0300

    v3.0: Vector<T> konstruktorius ir destruktorius

--- Vector.h
+++ Vector.h
@@ -3,6 +3,13 @@
 
 template<typename T>
 class Vector {
+public:
+    Vector() : data_(nullptr), size_(0), capacity_(0) {}
+
+    ~Vector() {
+        delete[] data_;
+    }
+
 private:
     T*     data_;
     size_t size_;
